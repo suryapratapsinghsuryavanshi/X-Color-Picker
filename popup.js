2a9d983aeb8261f45de3c0049a84297a7ad5f283
Chandrapratap Suryavanshi <73166278+chndrapratapsingh@users.noreply.github.com>
2023-01-11 09:00:15 +0530

    i write js

--- popup.js
+++ popup.js
@@ -1,1 +1,24 @@
 // JavaScript code for managing popup.
+
+let id = document.getElementById("sidebarbutton");
+
+id.addEventListener("click", sideBarOn)
+
+function sideBarOn(){
+    console.log("ggooo");
+    let button = document.getElementById("sidebarbutton");
+    let sidebar = document.getElementById("sideBarOff");
+    sidebar.removeAttribute("id");
+    sidebar.setAttribute("id","sideBarOn");
+    button.removeAttribute("onclick");
+    button.setAttribute("onclick","sideBarOf()");
+}
+
+function sideBarOf(){
+    let button = document.getElementById("sidebarbutton");
+    let sidebar = document.getElementById("sideBarOn");
+    sidebar.removeAttribute("id");
+    sidebar.setAttribute("id","sideBarOf");
+    button.removeAttribute("onclick");
+    button.setAttribute("onclick","sideBarOn()");
+}
