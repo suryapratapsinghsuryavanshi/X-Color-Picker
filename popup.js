324d98b29d20b383a4131e3b46f68f41663c62dc
Suryapratap Singh Suryavanshi <suryprtaps@gmail.com>
2023-01-11 22:04:47 +0530

    add color picking function

--- popup.js
+++ popup.js
@@ -1,11 +1,27 @@
 // JavaScript code for managing popup.
 
+// Color
+let current_selected_color = null;
+
+document.getElementById("color_code").innerText = "#ffb835";
+
+document.getElementById("up_arrow").addEventListener("click", () => {
+
+})
+
+function putColor(color) {
+    document.getElementById("colorDiv").style.backgroundColor = color;
+    document.getElementById("color_code").innerText = color;
+}
+
 let sideBarId = document.getElementById("sidebarbutton");
 let isToggle = false;
 
 sideBarId.addEventListener("click", () => {
     if(!isToggle) {
-        sideBarOn();
+        if(sideBarOn) {
+            sideBarOn();
+        }
         isToggle = true;
     }else {
         sideBarOf();
@@ -24,3 +40,15 @@ function sideBarOf(){
     sidebar.removeAttribute("id");
     sidebar.setAttribute("id","sideBarOff");
 }
+
+// Pick Color
+let pick_color = document.getElementById("color_pick");
+pick_color.addEventListener("click", () => {
+    let eye = new EyeDropper();
+    eye.open().then(r => {
+        current_selected_color = r;
+        putColor(current_selected_color.sRGBHex);
+    }).catch((e) => {
+        console.log("Error: " + e);
+    });
+});
