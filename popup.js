b83b3ea15bb6e3b0ec39aa4db84923b278ebe683
Suryapratap Singh Suryavanshi <suryprtaps@gmail.com>
2023-02-05 08:30:36 +0530

    add localStorage and hsv color foramte

--- popup.js
+++ popup.js
@@ -1,24 +1,35 @@
 // JavaScript code for managing popup.
 
+//  Utility
+const max = (list) => {
+    return list.sort()[list.length - 1];
+}
+
+const min = (list) => {
+    return list.sort()[0];
+}
+
 // LocalStorage
 let colorStore = "color_store_x_color_picker";
 
 // Color
 let current_selected_color = "#ffb835";
 let color_foramte_count = 0;
-let colorFromates = { hex: "#ffb835", rgb: "rgb(255, 184, 53)" };
+let colorFromates = { hex: "#ffb835", rgb: "rgb(255, 184, 53)", hsv: "hsv(39, 79, 100)" };
+
+let colorFormatesLists = [];
 
 document.getElementById("color_code").innerText = current_selected_color;
 
 document.getElementById("up_arrow").addEventListener("click", () => {
-    let temp_arr = [ "hex", "rgb" ];
+    let temp_arr = [ "hex", "rgb", "hsv" ];
     color_foramte_count += 1;
     let select = temp_arr[ color_foramte_count % temp_arr.length ];
     document.getElementById("color_code").innerText = colorFromates[select];
 });
 
 document.getElementById("down_arrow").addEventListener("click", () => {
-    let temp_arr = [ "hex", "rgb" ];
+    let temp_arr = [ "hex", "rgb", "hsv" ];
     if(color_foramte_count >= 2) {
         color_foramte_count -= 1;
     }
@@ -65,8 +76,10 @@ pick_color.addEventListener("click", () => {
     eye.open().then(r => {
         colorFromates.hex = r.sRGBHex;
         colorFromates.rgb = hexToRgb(r.sRGBHex);
+        colorFromates.hsv = hexToHsv(r.sRGBHex);
         putColor("hex");
-        localStorage.setItem(color_store_x_color_picker, JSON.stringify(colorFromates));
+        colorFormatesLists.push(JSON.stringify(colorFromates));
+        localStorage.setItem(colorStore, JSON.stringify(colorFormatesLists));
     }).catch((e) => {
         console.log("Error: " + e);
     });
@@ -77,4 +90,39 @@ const hexToRgb = (hex) => {
     let [has, r, g, b] = [hex.substr(0, 1), hex.substr(1, 2), hex.substr(3, 2), hex.substr(5, 2)]
     console.log(`rgb(${parseInt(r, 16)}, ${parseInt(g, 16)}, ${parseInt(b, 16)})`)
     return `rgb(${parseInt(r, 16)}, ${parseInt(g, 16)}, ${parseInt(b, 16)})`
+}
+const hexToHsv = (hex) => {
+    let [has, r, g, b] = [hex.substr(0, 1), hex.substr(1, 2), hex.substr(3, 2), hex.substr(5, 2)]
+    let [ R, G, B ] = [ parseInt(r, 16), parseInt(g, 16), parseInt(b, 16) ];
+    let [ R_, G_, B_ ] = [ R / 255, G / 255, B / 255 ];
+    let [ cMax, cMin ] = [ max([ R_, G_, B_ ]), min([ R_, G_, B_ ]) ];
+    let delta = cMax - cMin;
+
+    // for Hue
+    let HUE = null;
+    if(delta === 0) {
+        HUE = 0;
+    }else if(cMax === R_) {
+        HUE = 60 * (( (G_ - B_) / delta ) % 6);
+    }else if(cMax === G_) {
+        HUE = 60 * (( (B_ - R_) / delta ) + 2);
+    }else if(cMax === B_) {
+        HUE = 60 * (( (R_ - G_) / delta ) + 4);
+    }else {
+        console.log("Wrong Color Formate.");
+    }
+
+    // for Saturation 
+    let SATURATION = null;
+    if(cMax === 0) {
+        SATURATION = 0;
+    }else if(cMax !== 0) {
+        SATURATION = delta / cMax;
+    }
+
+    // for Value
+    let VALUE = null;
+    VALUE = cMax;
+    console.log(Math.round(HUE), Math.round(SATURATION * 100), Math.round(VALUE * 100));
+    return `hsv(${Math.round(HUE)}, ${Math.round(SATURATION * 100)}, ${Math.round(VALUE * 100)})`
 }
